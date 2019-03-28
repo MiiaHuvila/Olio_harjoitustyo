1adeb47740c75f3ea3024a78811e56ab7ecbfa00
Miia Huvila <Miia.Huvila2@centria.fi>
2019-03-28 11:58:28 +0200

    Lisätään Karsina-luokka

---
+++ Olio_harjoitustyo/Karsina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_harjoitustyo
+{
+    class Karsina
+    {
+        //Karsina-luokan ominaisuudet
+        String kuivike;
+
+        //Karsina-luokan metodit
+    }
+}

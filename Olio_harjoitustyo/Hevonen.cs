3df076e87f79ed0e86053900eb158c7d1e89b8b2
Miia Huvila <Miia.Huvila2@centria.fi>
2019-03-28 12:18:49 +0200

    Hevonen-luokan metodien lisäys

--- Olio_harjoitustyo/Hevonen.cs
+++ Olio_harjoitustyo/Hevonen.cs
@@ -8,11 +8,28 @@ namespace Olio_harjoitustyo
 {
     class Hevonen
     {
-        //Hevonen-luokan ominaisuudet
+        /*  Hevonen-luokan ominaisuudet */
         String hevonenNimi;
         String rotu;
 
-        //Hevonen-luokan metodit
-
+        /*  Hevonen-luokan metodit  */
+        //tiedon tuominen olioon
+        void laitaHevonenNimi(string hn)
+        {
+            hevonenNimi = hn;
+        }
+        void laitaHevonenRotu(string r)
+        {
+            rotu = r;
+        }
+        //tiedon vieminen oliosta
+        string vieHevonenNimi()
+        {
+            return hevonenNimi;
+        }
+        string vieHevonenRotu()
+        {
+            return rotu;
+        }
     }
 }

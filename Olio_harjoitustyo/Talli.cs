72715b354b9f2c6c65dcbdb7262998804fdc5904
Miia Huvila <Miia.Huvila2@centria.fi>
2019-03-28 12:06:09 +0200

    Lisätään Talli-luokka

---
+++ Olio_harjoitustyo/Talli.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_harjoitustyo
+{
+    class Talli
+    {
+        //Talli-luokan ominaisuudet
+        String talliNimi;
+        String paikkakunta;
+        String puh;
+
+        //Talli-luokan metodit
+
+    }
+}
